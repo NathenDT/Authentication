cb06a0a311bef09e35c1f134a5ec00bbabf9c0f6
Nathen dela Torre <35028757+N20Code@users.noreply.github.com>
2022-11-23 09:09:31 -0800

    more

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -32,9 +32,9 @@ const MY_SQL_URL = process.env.MY_SQL_URL as string
   app.use(express.json())
   app.use(
     cors({
-      origin: ['http://localhost:3000/', 'https://nathendt.github.io/'],
+      origin: ['http://localhost:3000', 'https://nathendt.github.io/'],
       credentials: true,
-      optionsSuccessStatus: 401,
+      optionsSuccessStatus: 200,
     })
   )
 
